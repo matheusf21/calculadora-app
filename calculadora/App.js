ef4fc9d14acf59ba1d2e2182d48678ce3f76f3f7
matheusf21 <matheusferreiradelima21@gmail.com>
2026-02-25 10:47:34 -0300

    V0.3

--- calculadora/App.js
+++ calculadora/App.js
@@ -10,9 +10,34 @@ export default function App() {
   const[mode, setMode] =useState ("dark");
   const theme = themes[mode];
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor : theme.bg}]}>
+      <StatusBar barStyle={mode === "dark" ? "light-content" : "dark-content"} />
+
+      <View style={styles.topBar}>
+
+        <Pressable
+          onPress={() => setMode((m) => (m=== "dark" ? "light" : "dark"))}
+          styles={({pressed}) =>  [
+            styles.toggle,
+            {
+              backgroundColor: theme.card, 
+              opacity: pressed ? 0.75 : 1,
+              borderColor: theme.stroke
+            }
+          ]}
+        >
+          <Text style= {{ color: theme.text, fontWeight: "700"}}>
+            {mode === "dark" ? "Escuro": "Claro"}
+          </Text>
+        </Pressable>
+      </View>
+
+      <Display
+        theme={theme}
+        expression={state.expression}
+        value={state.display}
+      />
+      
     </View>
   );
 }
@@ -20,8 +45,26 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 18,
+    paddingTop: 40,
   },
+  topBar: {
+    alignItems: "flex-end",
+    marginBottom: 6,
+  },
+  toggle: {
+    paddingHorizontal: 14,
+    paddingVertical: 8,
+    borderRadius: 14,
+    borderWidth: 1,
+  },
+  pad: {
+    gap: 14,
+    paddingBottom: 18,
+  },
+  row: {
+    flexDirection: 'row',
+    gap: 14,
+  }
 });
